1082b5e7d6e079ee1003a6044dfbee1a3ff64373
RajputSneha17 <sneharajput63154@gmail.com>
2025-07-26 11:10:31 +0530

    new

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -5,7 +5,7 @@ export const StoreContext = createContext(null);
 
 export const StoreContextProvider = ({ children }) => {
   const [cartItems, setCartItems] = useState({});
-  const url = "http://localhost:4000";
+  const url = "https://food-delivery-5nzz.onrender.com";
 
   const [token, setToken] = useState(() => localStorage.getItem("token") || "");
   const [user, setUser] = useState(() => {
